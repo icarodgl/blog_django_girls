28a4ae26ed1244526cb0aab8026d0c8a80e441fc
icarodgl <icarodgl@gmail.com>
2018-03-30 10:00:11 -0300

    usuarios

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from . import views
+from django.contrib.auth.forms import  *
 
 urlpatterns = [
     re_path(r'^$',views.post_list, name='post_list'),
+    re_path(r'^home/$',views.post_list, name='post_list'),
     re_path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     re_path(r'^post/new/$', views.post_new, name='post_new'),
     re_path(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    re_path('acconts/', include('django.contrib.auth.urls'),name='contas'),
+    re_path(r'^signup/$', views.signup, name='signup'),
+    re_path('logout/', views.logout, name='logout'),
+    re_path('profile/',views.profile, name='profile'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
-from .forms import PostForm
+from .forms import PostForm, ProfileForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate, logout
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -11,7 +14,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = Post.objects.get(id = pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -24,7 +27,7 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-
+@login_required
 def post_edit(request, pk):
     post = Post.objects.get(id = pk)
     if request.method == "POST":
@@ -38,3 +41,67 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('blog/list.html')
+    else:
+        form = UserCreationForm()
+    return render(request, 'blog/signup.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    
+@login_required
+def profile(request):
+
+    user = request.user
+    message = ""
+
+    current_info = {
+        'username': user.username,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'email': user.email,
+    }
+
+    # return HttpResponse("this is the user profile page for username '%s'. update first, last, email, username, and password here." % request.user.username)
+    if request.method == 'POST':
+
+        # create a form instance and populate it with data from the request
+        form = ProfileForm(request.POST)
+
+        if form.is_valid():
+
+            if form.cleaned_data['username'] != user.username:
+                user.username = form.cleaned_data['username']
+
+            if form.cleaned_data['first_name'] != user.first_name:
+                user.first_name = form.cleaned_data['first_name']
+
+            if form.cleaned_data['last_name'] != user.last_name:
+                user.last_name = form.cleaned_data['last_name']
+
+            if form.cleaned_data['email'] != user.email:
+                user.email = form.cleaned_data['email']
+
+            if form.cleaned_data['password'] != user.password and form.cleaned_data['password'] != "":
+                user.set_password(form.cleaned_data['password'])
+
+            user.save()
+            message += "Profile updated."
+
+            return render(request, 'accounts/profile.html', {'form': form, 'message': message})
+
+    else:
+        form = ProfileForm(current_info)
+
+    return render(request, 'accounts/profile.html', {'form': form})
